fbf6c4a01b63b0ceb5ba421caa3ad16d3d2487e3
tom_glb <goodMoring_glb@atguigu>
2020-01-01 16:24:46 +0800

    quarz

---
+++ src/com/ljk/Job/MyJob.java
@@ -0,0 +1,15 @@
+package com.ljk.Job;
+
+/**
+ * 任务
+ * @author Administrator
+ *
+ */
+public class MyJob {
+
+	
+	 public void doTask(){
+		 System.out.println("第一种实现方式");
+	 }
+	
+}
---
+++ src/com/ljk/Job/TestQuartz.java
@@ -0,0 +1,15 @@
+package com.ljk.Job;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestQuartz {
+	
+	
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+	
+	}
+	
+
+}
